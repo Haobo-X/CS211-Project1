7b2adffcb1bb32dbf8383f1640e3fccb42d9234a
Haobo-X <56521414+Haobo-X@users.noreply.github.com>
2020-10-19 19:23:30 -0700

    Update mygemm.c

--- mygemm.c
+++ mygemm.c
@@ -444,3 +444,170 @@ void optimal(const double* A, const double* B, double *C, const int n, const int
         }
     }
 }
+
+//Strassen’s algorithm
+void strassen(const double* A, const double* B, double *C, const int n)
+{
+    int newSize = n / 2;
+    
+    if (n == 2)
+    {
+        mulMatrix(A, B, C, 2);
+    }
+    else
+    {
+        int i, j;
+        
+        double * A_0_0 = (double *)malloc(sizeof(double) * newSize);
+        double * A_0_1 = (double *)malloc(sizeof(double) * newSize);
+        double * A_1_0 = (double *)malloc(sizeof(double) * newSize);
+        double * A_1_1 = (double *)malloc(sizeof(double) * newSize);
+        double * B_0_0 = (double *)malloc(sizeof(double) * newSize);
+        double * B_0_1 = (double *)malloc(sizeof(double) * newSize);
+        double * B_1_0 = (double *)malloc(sizeof(double) * newSize);
+        double * B_1_1 = (double *)malloc(sizeof(double) * newSize);
+        double * C_0_0 = (double *)malloc(sizeof(double) * newSize);
+        double * C_0_1 = (double *)malloc(sizeof(double) * newSize);
+        double * C_1_0 = (double *)malloc(sizeof(double) * newSize);
+        double * C_1_1 = (double *)malloc(sizeof(double) * newSize);
+         
+        double * M_1 = (double *)malloc(sizeof(double) * newSize);
+        double * M_2 = (double *)malloc(sizeof(double) * newSize);
+        double * M_3 = (double *)malloc(sizeof(double) * newSize);
+        double * M_4 = (double *)malloc(sizeof(double) * newSize);
+        double * M_5 = (double *)malloc(sizeof(double) * newSize);
+        double * M_6 = (double *)malloc(sizeof(double) * newSize);
+        double * M_7 = (double *)malloc(sizeof(double) * newSize);
+        double * AResult = (double *)malloc(sizeof(double) * newSize);
+        double * BResult = (double *)malloc(sizeof(double) * newSize);
+        
+        for (i = 0; i < n / 2; i++) 
+        {
+            for (j = 0; j < n / 2; j++) 
+            {
+                A_0_0[i * n + j] = A[i * n + j];
+                A_0_1[i * n + j] = A[i * n + (j + n / 2)];
+                A_1_0[i * n + j] = A[(i + n / 2) * n + j];
+                A_1_1[i * n + j] = A[(i + n / 2) * n + (j + n / 2)];
+                
+                B_0_0[i * n + j] = B[i * n + j];
+                B_0_1[i * n + j] = B[i * n + (j + n / 2)];
+                B_1_0[i * n + j] = B[(i + n / 2) * n + j];
+                B_1_1[i * n + j] = B[(i + n / 2) * n + (j + n / 2)];            }
+        }
+        
+        addMatrix(A_0_0, A_1_1, AResult, newSize);
+        addMatrix(B_0_0, B_1_1, BResult, newSize);
+        strassen(AResult, BResult, M_1, newSize);
+        
+        addMatrix(A_1_0, A_1_1, AResult, newSize);
+        strassen(AResult, B_0_0, M_2, newSize);
+        
+        subMatrix(B_0_1, B_1_1, BResult, newSize);
+        strassen(A_0_0, BResult, M_3, newSize);
+        
+        subMatrix(B_1_0, B_0_0, BResult, newSize);
+        strassen(A_1_1, BResult, M_4, newSize);
+        
+        addMatrix(A_0_0, A_0_1, AResult, newSize);
+        strassen(AResult, B_1_1, M_5, newSize);
+        
+        subMatrix(A_1_0, A_0_0, AResult, newSize);
+        addMatrix(B_0_0, B_0_1, BResult, newSize);
+        strassen(AResult, BResult, M_6, newSize);
+        
+        subMatrix(A_0_1, A_1_1, AResult, newSize);
+        addMatrix(B_1_0, B_1_1, BResult, newSize);
+        strassen(AResult, BResult, M_7, newSize);
+        
+        //C11 = M1 + M4 - M5 + M7;
+        addMatrix(M_1, M_4, AResult, newSize);
+        subMatrix(M_7, M_5, BResult, newSize);
+        addMatrix(AResult, BResult, C_0_0, newSize);
+        
+        //C12 = M3 + M5;
+        addMatrix(M_3, M_5, C_0_1, newSize);
+        
+        //C21 = M2 + M4;
+        addMatrix(M_2, M_4, C_1_0, newSize);
+        
+        //C22 = M1 + M3 - M2 + M6;
+        addMatrix(M_1, M_3, AResult, newSize);
+        subMatrix(M_6, M_2, BResult, newSize);
+        addMatrix(AResult, BResult, C_1_1, newSize);
+        
+        for (i = 0; i < n / 2; i++) 
+        {
+            for (j = 0; j < n / 2; j++) 
+            {
+                C[i * n + j] = C_0_0[i * n + j];
+                C[i * n + (j + n / 2)] = C_0_1[i * n + j];
+                C[(i + n / 2) * n + j] = C_1_0[i * n + j];
+                C[(i + n / 2) * n + (j + n / 2)] = C_1_1[i * n + j];
+            }
+        }
+        free(A_0_0);
+        free(A_0_1);
+        free(A_1_0);
+        free(A_1_1);
+        free(B_0_0);
+        free(B_0_1);
+        free(B_1_0);
+        free(B_1_1);
+        free(C_0_0);
+        free(C_0_1);
+        free(C_1_0);
+        free(C_1_1);
+        free(M_1);
+        free(M_2);
+        free(M_3);
+        free(M_4);
+        free(M_5);
+        free(M_6);
+        free(M_7);
+        free(AResult);
+        free(BResult);
+    }
+}
+
+void addMatrix(const double* A, const double* B, double* C, const int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {
+            C[i * n + j] = A[i * n + j] + B[i * n + j];
+        }
+    }
+}
+
+void subMatrix(const double* A, const double* B, double* C, const int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {
+            C[i * n + j] = A[i * n + j] - B[i * n + j];
+        }
+    }
+}
+
+void mulMatrix(const double* A, const double* B, double* C, const int n)
+{
+    int i, j, k;
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {
+            C[i * n + j] = 0;
+            for (k = 0; k < n; k++) 
+            {
+                C[i * n + j] += A[i * n + k] * B[k * n + j];
+            }
+        }
+    }
+}
+
+
